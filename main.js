4510d99047b7fadce0cb2f9ab582052df9dfe07b
pedro <ph.honorato28@gmail.com>
2024-06-10 23:06:12 -0300

    feat: enhance responsivity

--- main.js
+++ main.js
@@ -3,3 +3,11 @@ $(function () {
   $("#info").load("src/sections/info/info.html");
   $("#media").load("src/sections/media/media.html");
 });
+
+// TODO:
+// Terminar a responsividade
+// Trocar as fontes
+// Adicionar os ícones
+// Colocar botão de whatsapp no canto inferior direito / botão para mobile
+// Adicionar uma animação para os textos aparecerem conforme o scroll
+// Trocar a scrollbar
