19e2e3e51f0f27d61cc44335bd1c3421b99612a1
pedro <ph.honorato28@gmail.com>
2024-06-15 23:30:03 -0300

    feat: added version to layout

--- main.js
+++ main.js
@@ -5,10 +5,16 @@ $(function () {
   $("#info").load("src/sections/info/info.html");
   $("#media").load("src/sections/media/media.html");
   $("#whatsapp").load("src/sections/whatsapp/whatsapp.html");
+  $("#footer").load("src/sections/footer/footer.html");
 });
 
+function loadWebVersion() {
+  $("#version").empty().html(`v${version}`);
+}
+
+window.setTimeout(loadWebVersion, 500);
+
 // TODO:
-// adicionar um footer
 // Corrigir alinhamento em mobile
 // Adicionar uma animação para os textos aparecerem conforme o scroll
 // Trocar a scrollbar
